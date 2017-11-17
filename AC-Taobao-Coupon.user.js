08f5393404644d9904fa7ed4e56e4df512ab8ac5
langren1353 <1353464539@qq.com>
2017-11-17 13:02:05 +0800

    修复优惠券获取不正确的问题，以及部分情况下无法显示优惠券的问题
    Signed-off-by: langren1353 <1353464539@qq.com>

--- AC-Taobao-Coupon.user.js
+++ AC-Taobao-Coupon.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         AC-淘宝天猫商品优惠信息查询
-// @version      2.4
+// @version      2.7
 // @description  淘宝天猫商品查询是否具有优惠券
 // @author       AC
 // @include      https://item.taobao.com/item.htm*
 // @include      https://detail.tmall.com/item.htm*
 // @include      https://s.taobao.com/search?q=*
+// @note         2017.11.17-V2.7 修复优惠券获取不正确的问题，以及部分情况下无法显示优惠券的问题
+// @note         2017.11.12-V2.6 简单更新
+// @note         2017.11.11-V2.5 查询速度更快速，并且优化了载入情况，加入了个人数据
 // @note         2017.11.10-V2.4 如果有优惠，可以展示出优惠信息了
 // @note         2017.11.4-V2.3 暂时新增两种模式供选择
 // @note         2017.11.2-V2.2 参照了一个大神的脚本之后拿到了一些css，于是又加了些东西，展示一些双十一相关的标签
@@ -22,10 +25,12 @@
 var acJquery = jQuery.noConflict();
 function AutoStart(time, cssSelector, dealFunc) {
     var timerNode = setInterval(function () {
-        if (document.querySelector(cssSelector).style.display == "") {
-            clearInterval(timerNode);
-            dealFunc();
-        }
+        try{
+            if (document.querySelector(cssSelector).style.display == "") {
+                clearInterval(timerNode);
+                dealFunc();
+            }
+        }catch (e){}
     }, time);
 }
 function getQueryString(name) {
@@ -46,11 +51,31 @@ function acBuyScript(result){
         node.innerHTML = "！有优惠券啊！";
         node.href = "https://s.click.taobao.com/" + result.mod_json_details.couponUrl;
     }else{
-        node.innerHTML = "已领取或无优惠"
+        node.innerHTML = "已领取或无优惠";
     }
 }
+function relaad(result){
+    if(result.count == 0){
+        if(result.mod_json_details.couponUrl != ""){
+            location.href = "https://s.click.taobao.com/"+result.mod_json_details.couponUrl;
+        }
+    }
+    acBuyScript(result);
+}
+function queryData(goodID, type){
+    setTimeout(function(){
+        acJquery.ajax({
+            url:"https://www.ntaow.com/getDetails_json.jsp?func="+type+"&auctionId="+goodID,
+            dataType:'jsonp',
+            success:function(result) {
+                console.log(result);
+            },
+            timeout:3000
+        });
+    }, 200);
+}
 if (location.host == "item.taobao.com" || location.host == "detail.tmall.com") {
-    AutoStart(200, ".tb-detail-hd, .tb-main-title", function () {
+    AutoStart(100, ".tb-detail-hd, .tb-main-title", function () {
         var TitleNode = document.querySelector("div#J_Title h3, div.tb-detail-hd h1");
         var goodTitle = TitleNode.lastChild.nodeValue.trim();
         var goodID = getQueryString("id");
@@ -63,16 +88,11 @@ if (location.host == "item.taobao.com" || location.host == "detail.tmall.com") {
         htmlText += "&nbsp;&nbsp;&nbsp;&nbsp;<a href=" + querySimilar + " target='_blank' style='color: red;'>==找相似==</a>";
         insNode.innerHTML = htmlText;
         faNode.appendChild(insNode);
-        setTimeout(function(){
-            acJquery.ajax({
-                url:"https://www.ntaow.com/getDetails_json.jsp?auctionId="+goodID,
-                dataType:'jsonp',
-                success:function(result) {
-                    console.log(result);
-                },
-                timeout:3000
-            });
-        }, 200);
+        if(location.href.indexOf("ali_trackid") > 0){
+            queryData(goodID, "acBuyScript");
+        }else{
+            queryData(goodID, "relaad");
+        }
         var jsNode = document.createElement("script");
         jsNode.innerHTML = "function acBuyScript(result){\n" +
             "    var node = document.querySelector(\".acBuyScriptCoupon\");\n" +
@@ -80,9 +100,16 @@ if (location.host == "item.taobao.com" || location.host == "detail.tmall.com") {
             "        node.innerHTML = \"！有优惠券啊！\";\n" +
             "        node.href = \"https://s.click.taobao.com/\" + result.mod_json_details.couponUrl;\n" +
             "    }else{\n" +
-            "        node.innerHTML = \"无优惠信息\"\n" +
-            "        node.style = \"font-size: 11px;\";\n" +
+            "        node.innerHTML = \"已领取或无优惠\";\n" +
             "    }\n" +
+            "}\n" +
+            "function relaad(result){\n" +
+            "    if(result.count == 0){\n" +
+            "        if(result.mod_json_details.couponUrl != \"\"){\n" +
+            "            location.href = \"https://s.click.taobao.com/\"+result.mod_json_details.couponUrl;\n" +
+            "        }\n" +
+            "    }\n" +
+            "    acBuyScript(result);\n" +
             "}";
         document.body.appendChild(jsNode);
     });
