fa91618d7fca7ecc4191b0c2298d61109818cfe7
langren1353 <1353464539@qq.com>
2018-05-16 20:15:29 +0800

    更新：完全切换为搜狗翻译的结果

--- Translate-Youdao.user.js
+++ Translate-Youdao.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name  AC-有道取词+翻译
 // @description 一个可以在浏览器中自由使用的屏幕取词脚本-alt+鼠标翻译，或者选中按Q翻译
-// @version 1.1
+// @version 1.5
 // @namespace youdao
 // @author  AC -modified from ：Liu Yuyang(sa@linuxer.me)
 // @include *
 // @icon    https://coding.net/u/zb227/p/zbImg/git/raw/master/img0/icon.jpg
 // @run-at  document-end
+// @note    V1.5 2018-05-16 修复英文翻译单词的bug，同时修复语音的问题，暂时就这样了，最近不更新了，有需要的话提出来，以后再说
+// @note    V1.4 2018-05-16 修复上一次更新导致的bug，同时修复多种语言翻译，结果却翻译成了英文的bug
+// @note    V1.3 2018-05-16 修复word词组的时候翻译出错的问题
+// @note    V1.2 2018-05-16 修复没有添加connect的错误问题
 // @note    V1.1 2018-05-12 有道自己不更新~~HTTPS证书过期，现在使用搜狗翻译吧，老司机们说搜狗翻译还是很有优势的，至于百度翻译，再说吧
 // @note    V1.0 2017-11-22 修复firefox57的翻译的BUG
 // @note    V0.9 2017-11-15 修复了一大堆，优化了下界面的展示问题
@@ -19,6 +23,7 @@
 // @note    V0.2 2015-11-26 修改部分地方适合自己使用，选中文字，按下Q翻译，或者alt+鼠标选中，自动翻译
 // @grant GM_xmlhttpRequest
 // @connect fanyi.youdao.com
+// @connect fanyi.sogou.com
 // ==/UserScript==
 
 var curX;
@@ -33,8 +38,8 @@ document.body.addEventListener("dblclick", callbackWrapper, false);
 
 function callbackWrapper(e) {
     if(e.type === 'dblclick' && isInACTransPop()){
-        console.log("双击操作：复制目标文本");
-        console.log(transResult);
+        // console.log("双击操作：复制目标文本");
+        // console.log(transResult);
         return;
     }
     if(e.charCode == 113  || e.type == "mouseup"){ //Q
@@ -73,13 +78,12 @@ function translate() {
     }
 
     function popup(mx, my, result) {
-
         var youdaoWindow = document.createElement('div');
         youdaoWindow.classList.toggle("ACyoudaoPopup");
         // parse
         var dictJSON = JSON.parse(result);
         console.log(dictJSON);
-        var query = dictJSON['translate']['orig_text'];
+        var query = dictJSON['translate']['text'];
         // var errorCode = dictJSON['errorCode'];
         console.log(query);
         if (dictJSON['dictionary']) {
@@ -117,19 +121,18 @@ function translate() {
         youdaoWindow.style.zIndex = '9999999999';
 
         function word() {
-
-            function play(word) {
-                //console.log("[DEBUG] PLAYOUND")
-
+            function play(soundURL) {
+                if(soundURL.indexOf("http") != 0) soundURL = "https:" + soundURL;
                 function playSound(buffer) {
                     var source = context.createBufferSource();
                     source.buffer = buffer;
                     source.connect(context.destination);
                     source.start(0);
                 }
-
                 var context = new AudioContext();
-                var soundUrl = 'https://dict.youdao.com/dictvoice?type=2&audio='+encodeURIComponent(word);
+                // var soundUrl = 'https://dict.youdao.com/dictvoice?type=2&audio='+encodeURIComponent(word);
+                console.log(soundURL);
+                var soundUrl = soundURL;
                 var p = new Promise(function(resolve, reject) {
                     var ret = GM_xmlhttpRequest({
                         method: "GET",
@@ -150,8 +153,7 @@ function translate() {
                     console.log(e);
                 });
             }
-
-            var basic = dictJSON['basic'];
+            var basic = dictJSON['basic'] != null ? dictJSON['basic']:dictJSON['dictionary']['content'][0];
             var header = document.createElement('div');
             header.className = "ACyoudaoPopupDIV";
             // header
@@ -161,20 +163,40 @@ function translate() {
             header.appendChild(span);
             // phonetic if there is
             var phonetic = basic['phonetic'];
-            if (phonetic) {
+            var transWord;
+            try{
+                transWord = basic['category'][0]['sense'][0]['description'];
+            }catch (e) {
+                transWord =  basic['usual'][0]['values'];
+            }
+            var phoneticText = [];
+            if(typeof(phonetic) == "object"){
+                var i = 0;
+                phonetic.map(function(one){
+                    phoneticText[i] = one['text']+";";
+                    i++;
+                });
+            }else{
+                phoneticText[0] = phonetic;
+            }
+            for(var i = 0; i < phoneticText.length; i++){
                 var phoneticNode = document.createElement('span');
-                phoneticNode.innerHTML = '[' + phonetic + ']';
+                if(i == phoneticText.length-1)
+                    phoneticNode.innerHTML = '[' + phoneticText[i] + ']：'+transWord;
+                else
+                    phoneticNode.innerHTML = '[' + phoneticText[i] + ']';
                 phoneticNode.style.cursor = "pointer";
                 header.appendChild(phoneticNode);
                 var playLogo = document.createElement('span');
                 header.appendChild(phoneticNode);
-                phoneticNode.addEventListener('mouseup', function(e){
-                    if (e.target === phoneticNode) {
-                    }
-                    e.stopPropagation();
-                    play(query);
-                }, false);
-            };
+                (function(index){
+                    phoneticNode.addEventListener('mousedown', function(e){
+                        if (e.target === phoneticNode) { }
+                        e.stopPropagation();
+                        play(phonetic[index]['filename']);
+                    }, false);
+                })(i);
+            }
             header.style.lineHeight = "1.8";
             header.style.color = "black";
             header.style.margin = "0";
@@ -195,7 +217,13 @@ function translate() {
             // ul style
             ul.style.margin = "0";
             ul.style.padding = "0";
-            basic['explains'].map(function(trans) {
+            var mapNode;
+            try{
+                mapNode = basic['category'][0]['sense'][0]['example'];
+            }catch (e) {
+                mapNode = basic['content'][0]['item']['core'][0]['branch'];
+            }
+            mapNode.map(function(trans) {
                 var li = document.createElement('li');
                 li.className = "ACyoudaoPopupLi";
                 li.style.listStyle = "none";
@@ -203,11 +231,16 @@ function translate() {
                 li.style.padding = "0";
                 li.style.background = "none";
                 li.style.color = "#008CD8";
-                li.appendChild(document.createTextNode(trans));
+                var innerHTML = "";
+                if(trans['source'] != undefined){
+                    innerHTML = trans['source']+":"+trans['target'];
+                }else{
+                    innerHTML = trans['detail']['zh']+":" + trans['detail']['en']+":";
+                }
+                li.appendChild(document.createTextNode(innerHTML));
                 ul.appendChild(li);
             });
             youdaoWindow.appendChild(ul);
-
         }
 
         function sentence() {
@@ -217,6 +250,7 @@ function translate() {
             ul.style.margin = "0";
             ul.style.padding = "0";
             var trans = dictJSON['translate']['dit'];
+            var oriText = dictJSON['translate']['text'];
             var li = document.createElement('li');
             li.style.listStyle = "none";
             li.style.margin = "0";
@@ -226,6 +260,8 @@ function translate() {
             li.className = "ACyoudaoPopupLi";
             trans = trans.replaceAll('QWER', '\n').replaceAll(",", ", ");
             console.log(trans);
+            if(oriText.length < 70 && !/\n/.test(oriText)) // 如果长度比较短，并且没有回车符
+                trans = oriText+"\n"+trans;
             transResult += trans;
             var reg = new RegExp("\n","gm");
             while(reg.exec(trans) != null){// 如果找不到回车了，那么最后重复一次新建<p>
@@ -238,7 +274,7 @@ function translate() {
                 newNode.style.background = "none";
                 newNode.style.color = "#008CD8";
                 newNode.style.fontSize = "15px";
-                newNode.innerHTML = trans.substring(0, reg.lastIndex-2);
+                newNode.innerHTML = trans.substring(0, reg.lastIndex-1);
                 li.appendChild(newNode);
                 trans = trans.substring(reg.lastIndex);
             }
@@ -252,18 +288,18 @@ function translate() {
         }
     }
 
-
     function translate(word, ts) {
+        var ToLANGUAGE = "en";
+        if(!/[\u4E00-\u9FA5]/g.test(word)) // 如果没有中文，那么说明是外文--> to=中文
+            ToLANGUAGE = "zh-CHS";
         word = encodeURIComponent(word);
         // var reqUrl = 'https://fanyi.youdao.com/openapi.do?type=data&doctype=json&version=1.1&relatedUrl=http%3A%2F%2Ffanyi.youdao.com%2F%23&keyfrom=fanyiweb&key=null&translate=on&q='+word+'&ts='+ts;
-        var reqUrl = 'https://fanyi.sogou.com/reventondc/translate?from=auto&to=en&client=pc&fr=browser_pc&useDetect=on&useDetectResult=on&text='+word+'&ts='+ts;
+        var reqUrl = 'https://fanyi.sogou.com/reventondc/translate?from=auto&to='+ToLANGUAGE+'&client=pc&fr=browser_pc&useDetect=on&useDetectResult=on&text='+word+'&ts='+ts;
         console.log("request url: ", reqUrl);
         var ret = GM_xmlhttpRequest({
             method: "GET",
             url: reqUrl,
             headers: {"Accept": "application/json"},  // can be omitted...
-            onreadystatechange: function(res) {
-            },
             onload: function(res) {
                 var retContent = res.response;
                 //console.log(retContent)
