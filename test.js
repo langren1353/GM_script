f54778d4cbfb5493b927a07c90a67c7b8c916aee
langren1353 <1353464539@qq.com>
2018-08-10 23:29:16 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        页面include标记测试
+// @author      AC
+// @namespace   666
+// @version     3.6
+// @require     https://code.jquery.com/jquery-1.9.0.min.js
+// @include     https://baidu.com/*
+// @include     https?://so.com/*
+// @updateURL   https://www.baidu.com/
+// @downloadURL   https://www.baidu.com/
+// @icon        https://coding.net/u/zb227/p/zbImg/git/raw/master/img0/icon.jpg
+// @run-at      document-end
+// ==/UserScript==
+// 页面TAG测试
+console.log("生效了");
