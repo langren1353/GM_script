5a86316e545a631db3c905286e5a43b143dade09
langren1353 <1353464539@qq.com>
2020-06-26 22:37:44 +0800

    修复Debian下的支持问题

--- shell/actcp.sh
+++ shell/actcp.sh
@@ -133,6 +133,7 @@ downAndInstallKernel(){
 
         # 提取kernel的version
         kernel_version=${res_image_name%-$use_img_version*}
+        kernel_version=${kernel_version%_*}
         echo "内核版本1：${kernel_version}"
         kernel_version=${kernel_version#kernel-}
         echo "内核版本2：${kernel_version}"
@@ -174,8 +175,9 @@ downAndInstallKernel(){
 
         # 提取kernel的version
         kernel_version=${res_image_name%-$use_img_version*}
+        kernel_version=${kernel_version%_*}
         echo "内核版本1：${kernel_version}"
-        kernel_version=${kernel_version#kernel-}
+        kernel_version=${kernel_version#linux-image-}
         echo "内核版本2：${kernel_version}"
 
         # 判断文件名是否有效，如果无效，return
@@ -207,7 +209,7 @@ downAndInstallKernel(){
 
 UpdateKernelAndRemoveOld(){
     cd .. && rm -rf bbr
-	detele_kernel
+  detele_kernel
 	BBR_grub
 	echo -e "${Tip} ${Red_font_prefix}请检查上面是否有内核信息，无内核千万别重启${Font_color_suffix}"
 	echo -e "${Tip} ${Red_font_prefix}rescue不是正常内核，要排除这个${Font_color_suffix}"
@@ -957,7 +959,7 @@ detele_kernel(){
 		if [ "${deb_total}" > "1" ]; then
 			echo -e "检测到 ${deb_total} 个其余内核，开始卸载..."
 			for((integer = 1; integer <= ${deb_total}; integer++)); do
-				deb_del=`dpkg -l|grep linux-image | awk '{print $2}' | grep -v "${kernel_version}" | head -${integer}`
+				deb_del=`dpkg -l | grep linux-image | awk '{print $2}' | grep -v "${kernel_version}" | head -${integer}`
 				echo -e "开始卸载 ${deb_del} 内核..."
 				apt-get purge -y ${deb_del}
 				echo -e "卸载 ${deb_del} 内核卸载完成，继续..."
