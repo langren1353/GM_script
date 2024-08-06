ca3eb3e870282b8d2c319bca49be1e7060826679
Desktop\3070 <1353464539@qq.com>
2024-08-06 12:08:08 +0800

    less.js功能修复，处理针对油猴脚本运行时head不存在的问题

--- Less4_1_2_fixed.js
+++ Less4_1_2_fixed.js
@@ -11305,37 +11305,17 @@
     var css;
     var head;
     var style;
-    // Always restore page visibility
     function resolveOrReject(data) {
         if (data.filename) {
             console.warn(data);
-        }
-        if (!options.async) {
-            head.removeChild(style);
         }
     }
     if (options.onReady) {
         if (/!watch/.test(window.location.hash)) {
             less.watch();
         }
-        // Simulate synchronous stylesheet loading by hiding page rendering
-        if (!options.async) {
-            css = 'body { display: none !important }';
-            head = document.head || document.getElementsByTagName('head')[0];
-            style = document.createElement('style');
-            style.type = 'text/css';
-            if (style.styleSheet) {
-                style.styleSheet.cssText = css;
-            }
-            else {
-                style.appendChild(document.createTextNode(css));
-            }
-            head.appendChild(style);
-        }
         less.registerStylesheetsImmediately();
         less.pageLoadFinished = less.refresh(less.env === 'development').then(resolveOrReject, resolveOrReject);
     }
-
     return less;
-
 })));
